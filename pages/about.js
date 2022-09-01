0b5849444f6126d1d0ce1834afacf8242926a3e1
Zeeshnhmd <zeeshnhmd.1@gmail.com>
2022-09-01 12:22:04 +0530

    f2_e3 using next-link to switch between routes

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,19 @@
+import Head from "next/head";
+import Link from "next/link";
 import React from "react";
-
+import styles from "../styles/Home.module.css";
 const about = () => {
-  return <div>hello form about</div>;
+  return (
+    <>
+      <Head>
+        <title>about page</title>
+      </Head>
+      <div className={styles.main}>
+        <h1>hello form about</h1>
+        <Link href="/">Home</Link>
+      </div>
+    </>
+  );
 };
 
 export default about;
