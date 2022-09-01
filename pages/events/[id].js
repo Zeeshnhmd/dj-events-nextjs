f1d73a12dae5c4869ff4c5074b2021a4979cfcac
Zeeshnhmd <zeeshnhmd.1@gmail.com>
2022-09-01 12:55:26 +0530

    f2_e3 getting the dynamic route name using useRouter

--- pages/events/[id].js
+++ pages/events/[id].js
@@ -1,9 +1,15 @@
 import React from "react";
+import { useRouter } from "next/router";
 
 export default function EventPage() {
+  const router = useRouter();
+  console.log(router);
   return (
     <div>
       <h1>EventPage dynamic routes</h1>
+      {/* we are using here (id) in router because we have name the file (id) if we have named the file (slug) then we have used (slug) */}
+      <h1>{router.query.id}</h1>
+      <button onClick={() => router.push("/")}>click</button>
     </div>
   );
 }
