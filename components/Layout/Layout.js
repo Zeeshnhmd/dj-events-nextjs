ba63e9aaa657b24b31a5271a6b4e277d78c709ea
Zeeshnhmd <zeeshnhmd.1@gmail.com>
2022-09-04 01:33:14 +0530

    f2_e8 added showcase component and images

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,9 +1,14 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
+import ShowCase from "../Showcase/ShowCase";
 import styles from "./Layout.module.css";
 
 export default function Layout({ title, keywords, description, children }) {
+  const router = useRouter();
+  console.log(router);
+
   return (
     <div>
       <Head>
@@ -13,6 +18,8 @@ export default function Layout({ title, keywords, description, children }) {
       </Head>
       <div className={styles.layout}>
         <Header />
+        {router.pathname === "/" && <ShowCase />}
+
         <div className={styles.container}>{children}</div>
         <div className={styles.footer}>
           <Footer />
---
+++ components/Showcase/ShowCase.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./ShowCase.module.css";
+
+const ShowCase = () => {
+  return (
+    <div className={styles.showcase}>
+      <h1>Welcome to the Party!</h1>
+      <h2>Find the Hottest DJ events</h2>
+    </div>
+  );
+};
+
+export default ShowCase;
