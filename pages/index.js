8fc6316493f7fad7a61df92645d0b52e4ae4de71
Zeeshnhmd <zeeshnhmd.1@gmail.com>
2022-09-05 16:23:26 +0530

    f3_e2 fetching data using getServerSideProps and getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,38 @@
 import Layout from "@/components/Layout/Layout";
+import { API_URL } from "config";
 
-export default function Home() {
+export default function Home({ events }) {
+  console.log(events);
   return (
     <Layout>
-      <h1>hello from home page</h1>
-      {/* <main className={styles.main}>
-        <h1>Hello from Home</h1>
-        <Link href="/about">About</Link>
-      </main> */}
+      <h1>Upcoming Events</h1>
     </Layout>
   );
 }
+
+// export async function getServerSideProps() {
+//   const res = await fetch(`${API_URL}/api/events`);
+//   const events = await res.json();
+
+//   console.log(events);
+
+//   return { props: { events } };
+// }
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  console.log(events);
+
+  return { props: { events }, revalidate: 1 };
+
+  /**
+   * * here what revalidate will do is that if it will find the request it request again after 1sec
+   */
+}
+
+/**
+ * * getServerSideProps run every time when we visit the page
+ * * getStaticProps runs on build time but we can also pass some options with props (revalidate, fallback) inside the return object
+ */
